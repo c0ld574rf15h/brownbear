c27aa8c302d19c286229e119cbd575be81541b89
c0ld574rf15h <c0ld574rf15h@gmail.com>
2020-01-01 11:30:14 +0900

    Added In House Challenge Component

---
+++ src/components/challenge/InHouse.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="in-house-challenges">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'inhouse-challenge',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
